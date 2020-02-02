d885efb5b072950e02ebbb93a6e3f05c66899261
Marcos <30581037+marcosvlt@users.noreply.github.com>
2020-02-02 18:59:52 -0300

    Create CelsiusToFahrenheit.py

---
+++ Algorithms/CelsiusToFahrenheit.py
@@ -0,0 +1,11 @@
+"""
+// Celsius to Fahrenheit
+// F = C *(9.0/5.0)+32.0
+"""
+
+C = float(input('Enter the temperature in Celsius \n'))
+
+F = C * (9.0 / 5.0) + 32.0
+round(F, 4)
+
+print(f'{C} to Fahrenheit if {F}')
