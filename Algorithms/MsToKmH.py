b27b268c615d62de4f15098afdf6e4c67bcbb874
Marcos <30581037+marcosvlt@users.noreply.github.com>
2020-02-09 11:13:18 -0300

    Create MsToKmH.py

---
+++ Algorithms/MsToKmH.py
@@ -0,0 +1,14 @@
+"""
+
+Meters per seconds to Kilometres per hour
+
+K = M * 3.6
+
+"""
+
+M = float(input(f'Please insert the Metre per seconds (m/h)\n'))
+
+K = M * 3.6
+
+print(f'{M} m/s is {round(K, 2)} Km/h')
+
